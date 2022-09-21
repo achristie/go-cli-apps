794f30d9913e9a7bc4ce7c750ee566342f3c9aa4
Andrew Christie <awchristie86@gmail.com>
2022-09-20 21:31:16 -0400

    add todo and tests

---
+++ ch1/go.mod
@@ -0,0 +1,3 @@
+module github.com/achristie/go-cli-apps/ch1
+
+go 1.19
---
+++ ch1/todo_test.go
@@ -0,0 +1,68 @@
+package todo_test
+
+import (
+	"os"
+	"testing"
+
+	todo "github.com/achristie/go-cli-apps/ch1"
+)
+
+func TestAdd(t *testing.T) {
+	l := todo.List{}
+
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
+	}
+}
+
+func TestComplete(t *testing.T) {
+	l := todo.List{}
+
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
+	}
+
+	if l[0].Done {
+		t.Errorf("New task should not be completed.")
+	}
+
+	l.Complete(1)
+
+	if !l[0].Done {
+		t.Errorf("New task should be completed.")
+	}
+}
+
+func TestSaveGet(t *testing.T) {
+	l1 := todo.List{}
+	l2 := todo.List{}
+
+	taskName := "New Task"
+	l1.Add(taskName)
+
+	if l1[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead.", taskName, l1[0].Task)
+	}
+
+	tf, err := os.CreateTemp("", "")
+
+	if err != nil {
+		t.Fatalf("Error creating temp file: %s", err)
+	}
+
+	defer os.Remove(tf.Name())
+
+	if err := l1.Save(tf.Name()); err != nil {
+		t.Fatalf("Error saving list to file: %s", err)
+	}
+
+	if err := l2.Get(tf.Name()); err != nil {
+		t.Fatalf("Error getting list from file: %s", err)
+	}
+}
